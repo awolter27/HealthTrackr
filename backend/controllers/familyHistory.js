2cb405362ae0743a60c0c13fa36df32d493c9d22
awolter27 <awolter@butler.edu>
2023-05-16 22:16:03 -0400

    familyHistory.js and healthConditions updated routes and seeded data

--- backend/controllers/familyHistory.js
+++ backend/controllers/familyHistory.js
@@ -2,10 +2,77 @@ const express = require('express');
 const router = express.Router();
 const { FamilyHistory } = require('../models');
 
+const seededData = [
+    {
+        relationship: "Paternal Grandfather",
+        living: [{
+            living: false,
+            age: 0
+        }],
+        deceased: [{
+            deceased: true,
+            ageAtDeath: 84
+        }],
+        healthCondition: "Colon Cancer",
+        ageOfDiagnosis: 79,
+        notes: "He underwent radiation, chemotherapy, and surgery."
+    },
+    {
+        relationship: "Father",
+        living: [{
+            living: true,
+            age: 61
+        }],
+        deceased: [{
+            deceased: false,
+            ageAtDeath: 0
+        }],
+        healthCondition: "Hypertension",
+        ageOfDiagnosis: 40,
+        notes: "He takes lisinopril."
+    },
+    {
+        relationship: "Mother",
+        living: [{
+            living: true,
+            age: 59
+        }],
+        deceased: [{
+            deceased: false,
+            ageAtDeath: 0
+        }],
+        healthCondition: "Osteoarthritis",
+        ageOfDiagnosis: 55,
+        notes: "She uses OTC naproxen."
+    }
+]
+
 router.get('', async (req, res, next) => {
     try {
         const myFamilyHistory = await FamilyHistory.find({});
         res.json(myFamilyHistory);
+    } catch (err) {
+        next();
+        console.log(err);
+    }
+})
+
+router.get('/seed', async (req, res, next) => {
+    try {
+        await FamilyHistory.deleteMany({});
+        await FamilyHistory.insertMany(seededData);
+        res.redirect('/familyhistory');
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+})
+
+router.post('/new', async (req, res, next) => {
+    try {
+        const newFamilyHistory = req.body;
+        await FamilyHistory.create(req.body);
+        res.redirect('/familyhistory');
     } catch (err) {
         next();
         console.log(err);
@@ -22,18 +89,7 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
-router.post('', async (req, res, next) => {
-    try {
-        const newFamilyHistory = req.body;
-        await FamilyHistory.create(req.body);
-        res.redirect('/familyhistory');
-    } catch (err) {
-        next();
-        console.log(err);
-    }
-})
-
-router.put('/:id', async (req, res, next) => {
+router.put('/:id/edit', async (req, res, next) => {
     try {
         const updatedFamilyHistory = await FamilyHistory.findByIdAndUpdate(req.params.id, req.body);
         res.redirect(`/familyhistory/${req.params.id}`);
@@ -43,7 +99,7 @@ router.put('/:id', async (req, res, next) => {
     }
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id/delete', async (req, res) => {
     try {
         const deletedFamilyHistory = await FamilyHistory.findByIdAndDelete(req.params.id);
         res.redirect('/familyhistory');
--- backend/controllers/healthConditions.js
+++ backend/controllers/healthConditions.js
@@ -2,10 +2,79 @@ const express = require('express');
 const router = express.Router();
 const { HealthConditions } = require('../models');
 
+const seededData = [
+    {
+        name: "Tooth Pain",
+        currentOrPast: [{
+            current: true,
+            past: false
+        }],
+        ageOfDiagnosis: 53,
+        symptoms: "Right Upper Molar Pain",
+        treatment: "OTC Pain Relief, Meloxicam",
+        notes: "I need to make an appointment to discuss this with the dentist."
+    },
+    {
+        name: "Osteoarthritis",
+        currentOrPast: [{
+            current: true,
+            past: false
+        }],
+        ageOfDiagnosis: 50,
+        symptoms: "Joint Pain",
+        treatment: "Meloxicam, Cortisone Injections",
+        notes: "I have an appointment with my orthopedic specialist to discuss the requirements for a knee replacement."
+    },
+    {
+        name: "Hypertension",
+        currentOrPast: [{
+            current: true,
+            past: false
+        }],
+        ageOfDiagnosis: 38,
+        symptoms: "None",
+        treatment: "Lifestyle Modifications, Amlodipine",
+        notes: "I've noticed my blood pressure slowly increasing since my last visit. I have an appointment with my PCP to discuss increasing the dose of my amlodipine."
+    },
+    {
+        name: "Chickenpox",
+        currentOrPast: [{
+            current: false,
+            past: true
+        }],
+        ageOfDiagnosis: 3,
+        symptoms: "Red, Itchy, Raised Rash",
+        treatment: "Symptomatic Care",
+        notes: "I was never vaccinated for varicella."
+    }
+]
+
 router.get('', async (req, res, next) => {
     try {
         const myHealthConditions = await HealthConditions.find({});
         res.json(myHealthConditions);
+    } catch (err) {
+        next();
+        console.log(err);
+    }
+})
+
+router.get('/seed', async (req, res, next) => {
+    try {
+        await HealthConditions.deleteMany({});
+        await HealthConditions.insertMany(seededData);
+        res.redirect('/healthconditions');
+    } catch (err) {
+        console.log(err);
+        next();
+    }
+})
+
+router.post('/new', async (req, res, next) => {
+    try {
+        const newHealthCondition = req.body;
+        await HealthConditions.create(req.body);
+        res.redirect('/healthconditions');
     } catch (err) {
         next();
         console.log(err);
@@ -22,18 +91,7 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
-router.post('', async (req, res, next) => {
-    try {
-        const newHealthCondition = req.body;
-        await HealthConditions.create(req.body);
-        res.redirect('/healthconditions');
-    } catch (err) {
-        next();
-        console.log(err);
-    }
-})
-
-router.put('/:id', async (req, res, next) => {
+router.put('/:id/edit', async (req, res, next) => {
     try {
         const updatedHealthCondition = await HealthConditions.findByIdAndUpdate(req.params.id, req.body);
         res.redirect(`/healthconditions/${req.params.id}`);
@@ -43,7 +101,7 @@ router.put('/:id', async (req, res, next) => {
     }
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id/delete', async (req, res) => {
     try {
         const deletedHealthCondition = await HealthConditions.findByIdAndDelete(req.params.id);
         res.redirect('/healthconditions');
