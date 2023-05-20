04997ac131b7e727e753c76de1af7aac5796b414
awolter27 <awolter@butler.edu>
2023-05-20 13:28:39 -0400

    VaccinationsIndex.js compelte with new design

--- frontend/src/pages/vaccinations/VaccinationsIndex.js
+++ frontend/src/pages/vaccinations/VaccinationsIndex.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function VaccinationsIndex() {
     const [vaccinations, setVaccinations] = useState([]);
@@ -22,49 +26,46 @@ function VaccinationsIndex() {
 
     function loaded(vaccinations) {
         return (
-            <>
-                <div className='index-container'>
-                    <h1 className='index-header'>Vaccinations</h1>
-                    <table className='table table-bordered table-hover'>
-                        <thead>
-                            <tr>
-                                <th>VACCINATION</th>
-                                <th>MANUFACTURER</th>
-                                <th>LOT NUMBER</th>
-                                <th>DATE</th>
-                                <th>NOTES</th>
-                                <th>ACTIONS</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {vaccinations.map((vaccination, idx) => {
-                                return (
-                                    <tr key={idx}>
-                                        <td>{vaccination.name}</td>
-                                        <td>{vaccination.manufacturer}</td>
-                                        <td>{vaccination.lotNumber}</td>
-                                        <td>{vaccination.date}</td>
-                                        <td>{vaccination.notes}</td>
-                                        <td>
-                                            <Link className='index-edit-link' to={'/vaccinations/:id/edit'}>
-                                                <h2 className='index-edit-text'>Edit</h2>
-                                            </Link>
-                                            <Link className='index-delete-link' to={'/vaccinations/:id/delete'}>
-                                                <h2 className='index-delete-text'>Delete</h2>
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                    <div className='index-new-container'>
-                        <Link className='index-new-link' to={'/vaccinations/new'}>
-                            <h2 className='index-new-text'>Add Vaccination</h2>
+            <Container fluid>
+                <Row className='py-5 justify-content-end'>
+                    <Col sm={4} className='text-center'>
+                        <h1 className='fs-1 fw-normal ms-sm-3'>Vaccinations</h1>
+                    </Col>
+                    <Col sm={4} className='text-center text-sm-end pe-sm-3'>
+                        <Link to={'/vaccinations/new'}>
+                            <button className='text-white fs-5 fw-light px-3 py-1 rounded-3' id='index-new-link'>Add Vaccination</button>
                         </Link>
-                    </div>
-                </div>
-            </>
+                    </Col>
+                </Row>
+                {vaccinations.map((vaccination, idx) => {
+                    return (
+                        <div className='d-flex justify-content-center'>
+                            <Card key={idx} border="dark" className='mb-4 text-center' id='card'>
+                                <Card.Header className='fs-3' id='card-header'>{vaccination.name}</Card.Header>
+                                <Card.Body>
+                                    <Card.Title className='fs-4'>Manufacturer</Card.Title>
+                                    <Card.Text className='fs-5 fw-light'>{vaccination.manufacturer}</Card.Text>
+                                    <Card.Title className='fs-4'>Lot Number</Card.Title>
+                                    <Card.Text className='fs-5 fw-light'>#{vaccination.lotNumber}</Card.Text>
+                                    <Card.Title className='fs-4'>Date Administered</Card.Title>
+                                    <Card.Text className='fs-5 fw-light'>{vaccination.date}</Card.Text>
+                                    <Card.Title className='fs-4'>Notes</Card.Title>
+                                    <Card.Text className='fs-5 fw-light'>{vaccination.notes}</Card.Text>
+                                    <Card.Title className='fs-4'>Actions</Card.Title>
+                                    <div className='d-flex justify-content-center'>
+                                        <Link className='me-3' to={'/vaccinations/:id/edit'}>
+                                            <button type="button" className='text-white fs-5 fw-light  px-3 py-1 rounded-3' id="index-edit-link">Edit</button>
+                                        </Link>
+                                        <Link className='ms-3' to={'/vaccinations/:id/delete'}>
+                                            <button type="button" className='text-white fs-5 fw-light  px-3 py-1 rounded-3' id="index-delete-link">Delete</button>
+                                        </Link>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    )
+                })}
+            </Container>
         )
     }
 
