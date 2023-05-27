1e8a580d9b5065e43a5fb63e15ff1e4621ea569c
awolter27 <awolter@butler.edu>
2023-05-26 20:56:06 -0400

    Added in medical icons into public, starting on homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <>
-            <h1 className="d-flex justify-content-center">Health Summary</h1>
+            <h1 className="d-flex justify-content-center py-5">Dashboard</h1>
         </>
     );
 }
